36483e2de77e3d0d2063b84cd3e9429989637710
RebeckaKinn <rebeckaki@getacademy.no>
2023-04-14 14:06:33 +0200

    Add project files.

---
+++ ShoppingCart/OrderLine.cs
@@ -0,0 +1,39 @@
+﻿namespace ShoppingCart
+{
+    public class OrderLine
+    {
+        private Product _product;
+        private int _count;
+
+        public OrderLine(Product newProduct, int quantity)
+        {
+            _product = newProduct;
+            _count = quantity;
+        }
+
+        public void RemoveItems(int itemsToRemove)
+        {
+            _count = _count - itemsToRemove;
+        }
+
+        public void AddItems(int itemsToAdd)
+        {
+            _count = _count + itemsToAdd;
+        }
+
+        public int ShowCount()
+        {
+            return _count;
+        }
+
+        public Product ShowProduct()
+        {
+            return _product;
+        }
+
+        public int TotalCount(Product product)
+        {
+            return _count * product.ShowPrice();
+        }
+    }
+}
---
+++ ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,51 @@
+﻿namespace ShoppingCart
+{
+    internal class ShoppingCart
+    {
+        public List<Product> Store = new List<Product>
+        {
+            new Product("A", 40),
+            new Product("B", 70),
+            new Product("C", 30),
+        };
+        public List<OrderLine> Cart = new List<OrderLine>();
+
+        public Product GetProductFromStore(string productName)
+        {
+            return Store.First(i => productName == i.ShowName());
+        }
+        public void AddToCart(Product product, int amount)
+        {
+            var newItem = new OrderLine(product, amount);
+            if (Cart.Contains(newItem)) newItem.AddItems(amount);
+            else Cart.Add(newItem);
+        }
+
+        public void RemoveFromCart(OrderLine product)
+        {
+            Cart.Remove(product);
+        }
+        public void ShowCart()
+        {
+            if (Cart.Count == 0)
+            {
+                Console.WriteLine("Handlekurven er tom.");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Handlekurv:");
+                int totalSum = 0;
+                foreach (OrderLine item in Cart)
+                {
+                    Console.WriteLine($"{item.ShowCount()} stk " +
+                        $"{item.ShowProduct().ShowName()} " +
+                        $"{item.ShowProduct().ShowPrice()} kr " +
+                        $"= {item.TotalCount(item.ShowProduct())}");
+                    totalSum += item.TotalCount(item.ShowProduct());
+                }
+                Console.WriteLine($"\nTotalt: {totalSum} kr.\n");
+            }
+        }
+    }
+}
